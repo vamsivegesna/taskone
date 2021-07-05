a8e9f5141e1a82389803324e94c4ec6e11c5760c
vamsiv <vamsi.krishnam@veranahealth.com>
2021-07-05 19:47:19 +0530

    Adding AWS DNS names

--- firstservice/src/main/java/com/taskone/firstservice/dto/UserInput.java
+++ firstservice/src/main/java/com/taskone/firstservice/dto/UserInput.java
@@ -1,17 +1,10 @@
 package com.taskone.firstservice.dto;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserInput {
 
-  @NotBlank(message = "Filed required.")
-  @Size(min = 2)
   private String name;
-
-  @NotNull(message = "Filed required.")
   private String surname;
 }
--- firstservice/src/main/java/com/taskone/firstservice/service/SecondServiceImpl.java
+++ firstservice/src/main/java/com/taskone/firstservice/service/SecondServiceImpl.java
@@ -2,11 +2,11 @@ package com.taskone.firstservice.service;
 
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,23 +15,21 @@ public class SecondServiceImpl implements SecondService {
 
   @Autowired private RestTemplate restTemplate;
 
+  @Value("${second.service.url}")
+  private String secondService;
+
   @Override
   public String getMessage() {
-    ResponseEntity<String> response = null;
-    try {
-      HttpHeaders headers = new HttpHeaders();
-      headers.setContentType(MediaType.APPLICATION_JSON);
-      HttpEntity<Void> requestEntity = new HttpEntity<>(null, headers);
-      response =
-          restTemplate.exchange(
-              "http://localhost:8082/secondservice/message",
-              HttpMethod.GET,
-              requestEntity,
-              String.class,
-              Collections.emptyMap());
-    } catch (Exception e) {
-
-    }
-    return response.getBody();
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<Void> requestEntity = new HttpEntity<>(null, headers);
+    return restTemplate
+        .exchange(
+            secondService + "/secondservice/message",
+            HttpMethod.GET,
+            requestEntity,
+            String.class,
+            Collections.emptyMap())
+        .getBody();
   }
 }
